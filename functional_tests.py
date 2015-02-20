215b2449331d715ca47470422847fc1a78831d01
markgreenwood <mrkjgreenwood@gmail.com>
2015-02-19 17:49:25 -0800

    Front page HTML now generated from a template.

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -30,11 +31,11 @@ class NewVisitorTest(unittest.TestCase):
 		
 		# She types "Buy peacock feathers" into a text box (Edith's hobby
 		# is tying fly-fishing lures)
-		inputbox.sendkeys('Buy peacock feathers')
+		inputbox.send_keys('Buy peacock feathers')
 
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do list
-		inputbox.sendkeys(Keys.ENTER)
+		inputbox.send_keys(Keys.ENTER)
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
