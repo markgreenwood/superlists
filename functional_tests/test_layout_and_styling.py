6ac0fa9809ca3a4ed176981d7fd51f40d817578e
markgreenwood <mrkjgreenwood@gmail.com>
2015-09-04 17:23:31 -0700

    Use form in all views, back to working state

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -18,7 +18,7 @@ class LayoutAndStylingTest(FunctionalTest):
         # She starts a new list and sees the input is nicely
         # centered there, too
         inputbox.send_keys('testing\n')
-        inputbox.get_item_input_box()
+        inputbox = self.get_item_input_box()
 
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
